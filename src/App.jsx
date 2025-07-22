199e1f40a949c4f6d6db721177b6696b362709e9
Abhishek Rathore <144262140+Abhu0407@users.noreply.github.com>
2025-07-22 13:58:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/pages/Hero'
+
+function App() {
+  return (
+    <main>
+      <Hero>
+
+      </Hero>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <div>
+        <main id='skills' className='py-6 px-4 sm:p-6 md:py-10 md:px-8'>
+
+        </main>
+    </div>
+  )
+}
+
+export default Skills
